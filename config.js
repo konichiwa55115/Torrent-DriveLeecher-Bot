a70aba492d2a64086fc69dd30994e82bc6a12dbd
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-10 14:06:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // const tgToken = "894269743:AAH3OAtoNulAMPwJg-Cp4zAGBxGazTlzn38"; //aryantestbot
-const tgToken = "1237067579:AAGIFKPpyPk9FGD55SpKN6CgZaiW8iEj2Io"; //gtorrent
+const tgToken = "5480351563:AAF4tVGfnAmWR90ttCAfhRLOU_hW8kFCeyc"; //gtorrent
 const downloadFolder = "downloads";
 const mongoUrl = "mongodb+srv://aryanvikash:vikash12@cluster0-mp4rw.mongodb.net/test?retryWrites=true&w=majority"
 // const maxSize = 10;
